01307f0b7003fc8efba8113bdf3cf5c980395421
Ankith-SR <136753619+Ankith-SR@users.noreply.github.com>
2023-06-20 01:36:48 +0530

    Time: 0 ms (100%), Space: 6.6 MB (77.84%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
@@ -1,16 +1,39 @@
 class Solution {
 public:
-    int maxNumberOfBalloons(string text) {
-        map<char, int> count;
-        map<char, int> balloon;
-        for(char c: text)
-            count[c]++;
-        for(char c: std::string("balloon"))
-            balloon[c]++;
-        
-        int res = text.length();
-        for(const auto &[key, value]: balloon)
-            res = min(res, count[key] / value);
-        return res;
+    int maxNumberOfBalloons(string s) 
+    {
+        string target = "balloon";
+
+        vector<int>v1(26,0);
+        for(int i=0;i<target.length();i++)
+        {
+            char ch = target[i];
+            v1[ch-'a']++;
+        }
+        vector<int>v2(26,0);
+        for(int i=0;i<s.length();i++)  
+        {
+            char ch = s[i];
+            v2[ch-'a']++;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(v1[i]==0)
+            {
+                continue;
+            }
+            else
+            {
+                v2[i] = v2[i] / v1[i];
+            }
+        }
+        int ans=INT_MAX;
+        unordered_set<char>st(target.begin(),target.end());
+        for(auto ch : st)
+        {
+            int val = ch - 'a';
+            ans=min(ans,v2[val]);
+        }
+        return ans;
     }
 };
