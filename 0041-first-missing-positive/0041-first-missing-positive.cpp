68453519bcbebe8296ce49e69fff995d52257381
Ankith-SR <136753619+Ankith-SR@users.noreply.github.com>
2023-06-23 12:34:27 +0530

    Time: 134 ms (23.87%), Space: 57 MB (19.39%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        unordered_map<int, int> ans;
+        if(nums.size() == 1){
+            if(nums[0] != 1){
+                return 1;
+            }
+            else{
+                return nums[0]+1;
+            }
+        }
+        for(auto x : nums){
+            ans[x]++;
+        }
+        for(int i = 1; i<nums.size()+1; i++){
+            if(ans[i]==0){
+                return i;
+            }
+        }
+        return *max_element(nums.begin(),nums.end())+1;
+    }
+};
