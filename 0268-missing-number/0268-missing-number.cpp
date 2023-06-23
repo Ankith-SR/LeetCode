11ce2b57ff4cf840d158f7a969b2479a1b3012f2
Ankith-SR <136753619+Ankith-SR@users.noreply.github.com>
2023-06-23 12:44:56 +0530

    Time: 25 ms (41.83%), Space: 18 MB (45.61%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,23 +1,9 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        unordered_map<int, int> ans;
-        if(nums.size() == 1){
-            if(nums[0] != 1){
-                return 1;
-            }
-            else{
-                return nums[0]-1;
-            }
-        }
-        for(auto x : nums){
-            ans[x]++;
-        }
-        for(int i = 1; i<nums.size()+1; i++){
-            if(ans[i]==0){
-                return i;
-            }
-        }
-        return 0;
+        int a = nums.size();
+        int n = accumulate(nums.begin(),nums.end(),0);
+        int add = (a*(a+1))/2;
+        return add-n;
     }
 };
