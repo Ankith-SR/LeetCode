a83720eb27a08b672dd86c123889b7adc0ec1a28
Ankith-SR <136753619+Ankith-SR@users.noreply.github.com>
2023-06-24 21:42:41 +0530

    Time: 12 ms (75.71%), Space: 6 MB (11.35%) - LeetHub

--- 2857-total-distance-traveled/2857-total-distance-traveled.cpp
+++ 2857-total-distance-traveled/2857-total-distance-traveled.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int distanceTraveled(int mainTank, int additionalTank) {
-        int temp = 0;
-        int ans=0;
-        int nice = mainTank;
-        while(nice>0){
-            nice -= 1;
-            temp += 1;
-            if(additionalTank>0){
-                if(temp%5 == 0 && temp != 0){
-                    ans += 1;
-                    nice += 1;
-                    additionalTank -= 1;
-                }
-            }
-        }
-        return mainTank*10 + ans*10;
+        int canTake = 0;
+        if(mainTank%4 != 0)
+            canTake = mainTank/4;
+        else
+            canTake = mainTank/4 - 1;
+        
+        if(canTake <= additionalTank)
+            return (mainTank + canTake)*10;
+        return (mainTank + additionalTank)*10;
+        
     }
 };
