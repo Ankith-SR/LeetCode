a87faa009ef66161a6d77e4da12a3ce781a673c9
Ankith-SR <136753619+Ankith-SR@users.noreply.github.com>
2023-06-26 10:46:42 +0530

    Time: 0 ms (100%), Space: 5.9 MB (46.07%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -6,7 +6,6 @@ public:
         int first = 1, last = x;
         while (first <= last) {
             int mid = first + (last - first) / 2;
-            // mid * mid == x gives runtime error
             if (mid  == x / mid)
                 return mid;
             else if (mid > x / mid) {
