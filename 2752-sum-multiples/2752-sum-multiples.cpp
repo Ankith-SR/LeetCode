2fb96cb231ae4b9a7295f898aa427d49a14bbbdc
Ankith-SR <136753619+Ankith-SR@users.noreply.github.com>
2023-07-03 11:41:17 +0530

    Time: 0 ms (100%), Space: 5.9 MB (93.17%) - LeetHub

--- 2752-sum-multiples/2752-sum-multiples.cpp
+++ 2752-sum-multiples/2752-sum-multiples.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int sumOfMultiples(int n) {
-        int sum = 0;
+        int ans = 0;
         for (int i = 3; i <= n; i++) {
             if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
-                sum += i;
+                ans += i;
             }
         }
-        return sum;
+        return ans;
     }
 };
 
