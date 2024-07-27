5632908620fa13b7e4d748f3f8fb03d55ba773f0
Ankith-SR <136753619+Ankith-SR@users.noreply.github.com>
2024-07-27 17:26:47 +0530

    Time: 3 ms (45.48%), Space: 7.7 MB (60.85%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans = "";
+        int i = 0;
+        while(i < word1.size() || i < word2.size()){
+            if( i < word1.size()){
+                ans += word1[i];
+            }
+            if(i < word2.size()){
+                ans += word2[i];
+            }
+            i++;
+        }
+        return ans;
+    }
+};
