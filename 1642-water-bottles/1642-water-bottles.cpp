153d89299d7c8b399f0c6c7f8d4753f399fd26c4
Ankith-SR <136753619+Ankith-SR@users.noreply.github.com>
2024-08-11 08:56:06 +0530

    Time: 0 ms (100%), Space: 7.4 MB (6.79%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int drank = 0;
-        while (numBottles >= numExchange)
-        {
-            int drinkable = numBottles / numExchange;
-            drinkable *= numExchange;
-            numBottles -= drinkable;
-            drank += drinkable;
-            numBottles += drinkable / numExchange;
-        }
-        drank += numBottles;
-        return drank;
+        return numBottles+(numBottles-1)/(numExchange-1);
     }
 };
