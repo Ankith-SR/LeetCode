1718462a72fa8c92e221fa26852c1054aa64a0a0
Ankith-SR <136753619+Ankith-SR@users.noreply.github.com>
2024-08-30 08:12:48 +0530

    Time: 145 ms (93.83%), Space: 72.2 MB (22.81%) - LeetHub

--- 1742-widest-vertical-area-between-two-points-containing-no-points/1742-widest-vertical-area-between-two-points-containing-no-points.cpp
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/1742-widest-vertical-area-between-two-points-containing-no-points.cpp
@@ -1,20 +1,15 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int maxWidthOfVerticalArea(std::vector<std::vector<int>>& points) {
-        // Sort the points based on their x-coordinates
-        std::sort(points.begin(), points.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
-            return a[0] < b[0];
-        });
-
-        int max_width = 0;
-        // Calculate the maximum difference between consecutive x-coordinates
-        for (int i = 1; i < points.size(); ++i) {
-            max_width = std::max(max_width, points[i][0] - points[i - 1][0]);
+    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+        vector<int> help;
+        for(int i = 0; i < points.size(); i++){
+            help.push_back(points[i][0]);
         }
-
-        return max_width;
+        sort(help.begin(), help.end());
+        int ans = 0;
+        for(int i = 1; i < help.size(); i++){
+            ans = max(ans, help[i]-help[i-1]);
+        }
+        return ans;
     }
 };
