f58a82362783d9144d61e6b3f9100d40eebe60f2
Ankith-SR <136753619+Ankith-SR@users.noreply.github.com>
2024-09-28 06:29:09 +0530

    Time: 0 ms (100%), Space: 8.4 MB (35.11%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -3,16 +3,15 @@ public:
     int divide(int dividend, int divisor) {
         if (dividend == INT_MIN && divisor == -1) 
             return INT_MAX;
-        if (dividend == 2147483647 && divisor == -1){
-            return -2147483647;
-        }
         unsigned int a = abs(dividend);
         unsigned int b = abs(divisor);
         unsigned int res = 0;
         
-        while(a >= b){
-            res++;
-            a -= b;
+        for (int x = 31; x >= 0; x--) {
+            if ((a >> x) >= b) {
+                a = a - (b << x);
+                res = res + (1 << x);
+            }
         }
         return (dividend > 0) == (divisor > 0) ? res : -res;
     }
