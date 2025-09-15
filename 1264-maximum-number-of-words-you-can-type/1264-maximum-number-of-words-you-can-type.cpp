85367a1475b2b850e81ce917919b0135fb05b48b
Ankith-SR <136753619+Ankith-SR@users.noreply.github.com>
2025-09-15 11:39:37 -0700

    Time: 0 ms (100%), Space: 9.3 MB (29.57%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
-        vector<string> s;
         istringstream iss(text);
         string word;
+        int count = 0;
+        bool ok;
 
         while(iss >> word){
-            s.push_back(word);
-        }
-
-        int count = s.size();
-
-        for(int i = 0; i < s.size(); i++){
-            for(int j = 0; j < s[i].size(); j++){
-                char let = s[i][j];
-                for(int k = 0; k < brokenLetters.size(); k++){
-                    if(let == brokenLetters[k]){
-                        count--;
-                        j = s[i].size();
-                        break;
-                    }
+            ok = true;
+            for(char c : word){
+                if(brokenLetters.find(c) != string::npos){
+                    ok = false;
+                    break;
                 }
             }
+            if(ok) count++;
         }
         return count;
     }
